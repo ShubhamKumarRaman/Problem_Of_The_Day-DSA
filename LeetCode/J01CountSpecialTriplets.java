4dec1db34ea51b1900719af13d85af55641d38ed
Shubham Kumar <shubham620409@gmail.com>
2025-12-09 19:14:49 +0530

    Using Count frequency to left and right element

--- LeetCode/J01CountSpecialTriplets.java
+++ LeetCode/J01CountSpecialTriplets.java
@@ -33,14 +33,45 @@ public class J01CountSpecialTriplets {
         return count;
     }
 
+    // Approach 2 - using count frequency
+    public static int usingCountFrequency(int[] nums) {
+        int length = nums.length;
+        int count = 0;
+        for (int i = 1; i < length - 1; i++) {
+            int leftCount = 0;
+            int rightCount = 0;
+            // Count left frequency
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] == nums[i] * 2) {
+                    leftCount++;
+                }
+            }
+
+            if (leftCount > 0) {
+                // Count right frequency
+                for (int j = i + 1; j < length; j++) {
+                    if (nums[j] == nums[i] * 2) {
+                        rightCount++;
+                    }
+                }
+            }
+
+            count += leftCount * rightCount;
+        }
+        return count;
+    }
+
     public static void main(String args[]) {
         int[] nums = { 6, 3, 6 };
-        System.out.println(usingTwoPointer(nums));
+        System.out.println("Using Two Pointer: " + usingTwoPointer(nums));
+        System.out.println("Using Count Frequency: " + usingCountFrequency(nums));
 
         int[] nums2 = { 8, 4, 2, 8, 4 };
-        System.out.println(usingTwoPointer(nums2));
+        System.out.println("Using Two Pointer: " + usingTwoPointer(nums2));
+        System.out.println("Using Count Frequency: " + usingCountFrequency(nums2));
 
         int[] nums3 = { 84, 2, 93, 1, 2, 2, 26 };
-        System.out.println(usingTwoPointer(nums3));
+        System.out.println("Using Two Pointer: " + usingTwoPointer(nums3));
+        System.out.println("Using Count Frequency: " + usingCountFrequency(nums3));
     }
 }
