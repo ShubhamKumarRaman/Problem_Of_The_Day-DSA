3fabae9da3323e841370aae0a2e21f1e637e67a6
Shubham Kumar <shubham620409@gmail.com>
2025-12-12 22:04:31 +0530

    Using Swapping

--- GFG/J03TransposeMatrix.java
+++ GFG/J03TransposeMatrix.java
@@ -16,6 +16,20 @@ public class J03TransposeMatrix {
         return result;
     }
 
+    // [Approach 2] Using constant space for Square Matrix O(n*n) Time and O(1)
+    // Space
+    public static int[][] usingSwaping(int[][] mat) {
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        return mat;
+    }
+
     public static void main(String args[]) {
         int[][] mat = {
                 { 1, 1, 1, 1 },
@@ -26,12 +40,14 @@ public class J03TransposeMatrix {
 
         ArrayList<ArrayList<Integer>> result = transpose(mat);
 
-        //Print result
-        for(ArrayList<Integer> row: result){
-            for(int el: row){
-                System.out.print(el+" ");
+        // Print result
+        for (ArrayList<Integer> row : result) {
+            for (int el : row) {
+                System.out.print(el + " ");
             }
             System.out.println();
         }
+        // using swapping
+        System.out.println(transpose(mat));
     }
 }
