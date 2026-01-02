4fe7290cb3be615b6607b4f490e0315c8d426294
ShubhamKumarRaman <shubham620409@gmail.com>
2026-01-02 10:23:59 +0530

    Using Set

--- LeetCode/2026-01/J02NRepeatedElement.java
+++ LeetCode/2026-01/J02NRepeatedElement.java
@@ -2,6 +2,7 @@
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 public class J02NRepeatedElement {
 
@@ -43,17 +44,32 @@ public class J02NRepeatedElement {
         return -1;
     }
 
+    //Using Set
+    public static int usingSet(int[] nums){
+        Set<Integer> set = new HashSet<>();
+
+        for(int i: nums){
+            if(!set.add(i)){
+                return i;
+            }
+        }
+        return nums[nums.length-1];
+    }
+
     public static void main(String args[]) {
         int[] nums = {5, 1, 5, 2, 5, 3, 5, 4};
         System.out.println("Result: " + usingSorting(nums));
         System.out.println("Using Hashtable: " + usingHashTable(nums));
+        System.out.println("Using Set: "+usingSet(nums));
         
         int[] nums2 = {1, 2, 3, 3};
         System.out.println("Result: " + usingSorting(nums2));
         System.out.println("Using HashTable: " + usingHashTable(nums2));
+        System.out.println("Using Set: "+usingSet(nums2));
         
         int[] nums3 = {2, 1, 2, 5, 3, 2};
         System.out.println("Result: " + usingSorting(nums3));
         System.out.println("Using HashTable: " + usingHashTable(nums3));
+        System.out.println("Using Set: "+usingSet(nums3));
     }
 }
