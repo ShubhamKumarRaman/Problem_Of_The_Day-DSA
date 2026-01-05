3f366a8f611505c2e5c70fc7560dc7005b760249
ShubhamKumarRaman <shubham620409@gmail.com>
2026-01-05 10:48:06 +0530

    Using Sliding window concept

--- GFG/2026-01/J05MaxSumSubArray.java
+++ GFG/2026-01/J05MaxSumSubArray.java
@@ -18,11 +18,30 @@ public class J05MaxSumSubArray {
         return result;
     }
 
+    //Approach 2- Sliding window
+    public static int maxSumSubArray2(int[] arr, int k) {
+        int n = arr.length;
+        int start = 0;
+        int end = k;
+        int current = 0;
+        for (int i = start; i < end; i++) {
+            current += arr[i];
+        }
+        int result = current;
+        while (end < n) {
+            current = (current - arr[start++] + arr[end++]);
+            result = result < current ? current : result;
+        }
+        return result;
+    }
+
     public static void main(String args[]) {
         int[] arr = {1, 4, 2, 10, 23, 3, 1, 0, 20};
         System.out.println("Max sum : " + maxSumSubArray(arr, 4));
+        System.out.println("Max sum : " + maxSumSubArray2(arr, 4));
 
         int[] arr2 = {100, 200, 300, 400};
         System.out.println("Max sm : " + maxSumSubArray(arr2, 1));
+        System.out.println("Max sm : " + maxSumSubArray2(arr2, 1));
     }
 }
